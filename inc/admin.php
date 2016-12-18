8c007c2b977804914b89037dfe2b3e63fdc8f29c
ernilambar <nilambar@outlook.com>
2016-12-18 16:07:44 +0545

    Initial commit

---
+++ inc/admin.php
@@ -0,0 +1,18 @@
+<?php
+
+add_action( 'admin_menu', 'ns_theme_check_admin_menu' );
+
+function ns_theme_check_admin_menu() {
+
+	add_theme_page( __( 'NS Theme Check', 'ns-theme-check' ), __( 'NS Theme Check', 'ns-theme-check' ), 'manage_options', 'ns-theme-check', 'ns_theme_check_render_admin_page', 'dashicons-tickets' );
+
+}
+
+function ns_theme_check_render_admin_page() {
+	?>
+	<div class="wrap">
+		<h1><?php esc_html_e( 'NS Theme Check', 'ns-theme-check' ); ?></h1>
+
+	</div><!-- .wrap -->
+	<?php
+}
---
+++ ns-theme-check.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:       NS Theme Check
+ * Plugin URI:        #
+ * Description:       Sniff Theme Check
+ * Version:           1.0.0
+ * Author:            Nilambar Sharma
+ * Author URI:        http://www.nilambar.net
+ * Text Domain:       ns-theme-check
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'NS_THEME_CHECK_BASENAME', basename( dirname( __FILE__ ) ) );
+define( 'NS_THEME_CHECK_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) );
+define( 'NS_THEME_CHECK_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
+
+require_once NS_THEME_CHECK_DIR . '/inc/admin.php';
