07cf207a7bfd83fa8229190e99a0792075876b19
Ulrich Pogson <grapplerulrich@gmail.com>
2017-03-04 23:54:54 +0100

    Move code around and add style.css header checks
    Add a way to check text domain of frameworks

---
+++ inc/checks.php
@@ -0,0 +1,236 @@
+<?php
+/**
+ * Theme Checks
+ *
+ * @package NS_Theme_Check
+ */
+
+/**
+ * Perform sniff check.
+ *
+ * @since 0.1.0
+ *
+ * @param string $theme_slug Theme slug.
+ * @param array  $args Arguments.
+ *
+ * @return bool
+ */
+function ns_theme_check_do_sniff( $theme_slug, $args = array() ) {
+
+	if ( ! file_exists( NS_THEME_CHECK_DIR . '/vendor/autoload.php' ) ) {
+		printf( esc_html__( 'It seems you are using GitHub provided zip for the plugin. Visit %1$sInstalling%2$s to find the correct bundled plugin zip.', 'ns-theme-check' ), '<a href="https://github.com/ernilambar/ns-theme-check#installing" target="_blank">', '</a>' );
+		return;
+	}
+
+	require_once NS_THEME_CHECK_DIR . '/vendor/autoload.php';
+
+	// Path to WordPress Theme coding standard.
+	PHP_CodeSniffer::setConfigData( 'installed_paths', NS_THEME_CHECK_DIR . '/vendor/wp-coding-standards/wpcs/', true );
+	PHP_CodeSniffer::setConfigData( 'csslint_path', NS_THEME_CHECK_DIR . '/node_modules/csslint/dist/cli.js --errors=errors', true );
+
+	// Set default standard.
+	PHP_CodeSniffer::setConfigData( 'default_standard', 'WordPress-Theme', true );
+
+	// Ignoring warnings when generating the exit code.
+	PHP_CodeSniffer::setConfigData( 'ignore_warnings_on_exit', true, true );
+
+	// Set text domains.
+	PHP_CodeSniffer::setConfigData( 'text_domain', implode( ',', $args['text_domains'] ), true );
+
+	if ( isset( $args['show_warnings'] ) ) {
+		PHP_CodeSniffer::setConfigData( 'show_warnings', absint( $args['show_warnings'] ), true );
+	}
+
+	PHP_CodeSniffer::setConfigData( 'testVersion', $args['minimum_php_version'] . '-7.0', true );
+
+	// Initialise CodeSniffer.
+	$phpcs_cli = new PHP_CodeSniffer_CLI();
+	$phpcs_cli->checkRequirements();
+
+	// Set CLI arguments.
+	$values['files']       = get_theme_root() . '/' . $theme_slug;
+	$values['reportWidth'] = '110';
+
+	if ( isset( $args['raw_output'] ) && 0 === absint( $args['raw_output'] ) ) {
+		$values['reports']['json'] = null;
+	}
+
+	if ( isset( $args['standard'] ) && ! empty( $args['standard'] ) ) {
+		$values['standard'] = $args['standard'];
+	}
+
+	// Ignore unrelated files from the check.
+	$values['ignored'] = array(
+		'.*/node_modules/.*',
+	);
+
+	ob_start();
+	$num_errors = $phpcs_cli->process( $values );
+	$raw_output = ob_get_clean();
+
+	// Sniff theme files.
+	if ( isset( $args['raw_output'] ) && 1 === absint( $args['raw_output'] ) ) {
+		echo '<pre>' . esc_html( $raw_output ) . '</pre>';
+	} else {
+		$output = json_decode( $raw_output );
+		if ( ! empty( $output ) ) {
+			ns_theme_check_render_json_report( $output );
+		}
+	}
+
+	// Has the theme passed?
+	if ( 0 === $num_errors ) {
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+/**
+ * Perform style.css header check.
+ *
+ * @since 0.3.0
+ *
+ * @param string $theme_slug Theme slug.
+ * @param array  $theme WP_Theme Theme object.
+ *
+ * @return bool
+ */
+function ns_theme_check_style_headers( $theme_slug, $theme ) {
+
+	$pass             = true;
+	$required_headers = array(
+		'Name',
+		'Description',
+		'Author',
+		'Version',
+		// 'License',
+		// 'License URI',
+		'TextDomain',
+	);
+
+	foreach ( $required_headers as $header ) {
+		if ( $theme->get( $header ) ) {
+			continue;
+		}
+		$notices[] = array(
+			/* translators: 1: comment header line, 2: style.css */
+			'message'  => sprintf(
+				__( 'The %1$s is not defined in the style.css header.', 'ns-theme-check' ),
+				$header
+			),
+			'severity' => 'error',
+		);
+	}
+
+	if ( strpos( $theme_slug, 'wordpress' ) || strpos( $theme_slug, 'theme' ) ) {
+		$notices[] = array(
+			'message'  => __( 'The theme name cannot contain WordPress or Theme.', 'ns-theme-check' ),
+			'severity' => 'error',
+		);
+	}
+
+	if ( preg_match( '|[^\d\.]|', $theme->get( 'Version' ) ) ) {
+		$notices[] = array(
+			'message' => __( 'Version strings can only contain numeric and period characters (like 1.2).', 'ns-theme-check' ),
+			'severity' => 'error',
+		);
+	}
+
+	// Prevent duplicate URLs.
+	$themeuri  = trim( $theme->get( 'ThemeURI' ) , '/\\' );
+	$authoruri = trim( $theme->get( 'AuthorURI' ) , '/\\' );
+	if ( $themeuri === $authoruri ) {
+		$notices[] = array(
+			'message'  => __( 'Duplicate theme and author URLs. A theme URL is a page/site that provides details about this specific theme. An author URL is a page/site that provides information about the author of the theme. The theme and author URL are optional.', 'ns-theme-check' ),
+			'severity' => 'error',
+		);
+	}
+
+	if ( $theme_slug === $theme->get( 'Text Domain' ) ) {
+		$notices[] = array(
+			/* translators: %1$s: Text Domain, %2$s: Theme Slug */
+			'message' => sprintf( __( 'The text domain "%1$s" must match the theme slug "%2$s".', 'ns-theme-check' ),
+				$theme->get( 'TextDomain' ),
+				$theme_slug
+			),
+			'severity' => 'error',
+		);
+	}
+
+	$registered_tags    = ns_theme_check_get_theme_tags();
+	$tags               = $theme->get( 'Tags' );
+	$tags_count         = array_count_values( $tags );
+	$subject_tags_names = array();
+
+	foreach ( $tags as $tag ) {
+		$tag = strtolower( $tag );
+
+		if ( $tags_count[ $tag ] > 1 ) {
+			$notices[] = array(
+				'message' => sprintf(
+					__( 'The tag "%s" is being used more than once, please remove the duplicate.', 'ns-theme-check' ),
+					$tag
+				),
+				'severity' => 'error',
+			);
+		}
+
+		if ( isset( $registered_tags['subject_tags'][ $tag ] ) ) {
+			$subject_tags_names[] = $tag;
+			continue;
+		}
+
+		if ( ! isset( $registered_tags['allowed_tags'][ $tag ] ) ) {
+			$notices[] = array(
+				'message' => sprintf(
+					__( 'Please remove "%s" as it is not a standard tag.', 'ns-theme-check' ),
+					$tag
+				),
+				'severity' => 'error',
+			);
+			continue;
+		}
+
+		if ( 'accessibility-ready' === $tag ) {
+			$notices[] = array(
+				'message' => __( 'Themes that use the "accessibility-ready" tag will need to undergo an accessibility review.', 'ns-theme-check' ),
+				'severity' => 'warning',
+			);
+		}
+	}
+
+	$subject_tags_count = count( $subject_tags_names );
+	if ( $subject_tags_count > 3 ) {
+		$notices[] = array(
+			'message' => sprintf(
+				__( 'A maximum of 3 subject tags are allowed. The theme has %1$d subjects tags [%2$s]. Please remove the subject tags, which do not directly apply to the theme.', 'ns-theme-check' ),
+				$subject_tags_count,
+				implode( ',', $subject_tags_names )
+			),
+			'severity' => 'error',
+		);
+	}
+
+	if ( empty( $notices ) ) {
+		return true;
+	}
+
+	?>
+	<div class="report-file-item">
+		<div class="report-file-heading">
+			<span class="heading-field"><?php printf( esc_html__( 'File: %s', 'ns-theme-check' ), $theme_slug . '/style.css' ); ?></span>
+		</div><!-- .report-file-heading -->
+		<table class="report-table">
+		<?php foreach ( $notices as $notice ) : ?>
+			<tr class="item-type-<?php echo esc_attr( $notice['severity'] ); ?>">
+			<td class="td-type"><?php echo esc_html( $notice['severity'] ); ?></td>
+			<td class="td-message"><?php echo esc_html( $notice['message'] ); ?></td>
+			</tr>
+		<?php endforeach; ?>
+		</table>
+	</div><!-- .report-file-item -->
+	<?php
+	return $pass;
+}
--- inc/helpers.php
+++ inc/helpers.php
@@ -6,250 +6,6 @@
  */
 
 /**
- * Render form.
- *
- * @since 0.1.0
- */
-function ns_theme_check_render_form() {
-
-	$standards = ns_theme_check_get_standards();
-
-	$all_themes = wp_get_themes();
-	$themes = array();
-
-	if ( ! empty( $all_themes ) ) {
-		foreach ( $all_themes as $key => $theme ) {
-			$themes[ $key ] = $theme->get( 'Name' );
-		}
-	}
-
-	if ( empty( $themes ) ) {
-		return;
-	}
-
-	$current_theme = get_stylesheet();
-
-	if ( ! empty( $_POST['themename'] ) ) {
-		$current_theme = $_POST['themename'];
-	}
-
-	$minimum_php_version = '5.2';
-
-	if ( ! empty( $_POST['minimum_php_version'] ) ) {
-		$minimum_php_version = $_POST['minimum_php_version'];
-	}
-
-	$hide_warning = 0;
-	if ( isset( $_POST['hide_warning'] ) && 1 === absint( $_POST['hide_warning'] ) ) {
-		$hide_warning = 1;
-	}
-
-	$raw_output = 0;
-	if ( isset( $_POST['raw_output'] ) && 1 === absint( $_POST['raw_output'] ) ) {
-		$raw_output = 1;
-	}
-
-	$standard_status = wp_list_pluck( $standards, 'default' );
-
-	if ( isset( $_POST['_wp_http_referer'] ) ) {
-		foreach ( $standards as $key => $standard ) {
-			if ( isset( $_POST[ $key ] ) && 1 === absint( $_POST[ $key ] ) ) {
-				$standard_status[ $key ] = 1;
-			} else {
-				$standard_status[ $key ] = 0;
-			}
-		}
-	}
-	?>
-	<form action="<?php echo esc_url( admin_url( 'themes.php?page=ns-theme-check' ) ); ?>" method="post" class="frm-theme-check">
-		<?php wp_nonce_field( 'ns_theme_check_run', 'ns_theme_check_nonce' ); ?>
-		<div class="theme-switcher-wrap">
-			<h2><?php esc_html_e( 'Select Theme', 'ns-theme-check' ); ?></h2>
-			<label for="themename">
-				<select name="themename">
-					<?php foreach ( $themes as $key => $value ) : ?>
-						<option value="<?php echo esc_attr( $key ); ?>" <?php selected( $current_theme, $key ); ?>><?php echo esc_html( $value ); ?></option>
-					<?php endforeach; ?>
-				</select>
-			</label>
-			<input type="submit" value="<?php esc_attr_e( 'GO', 'ns-theme-check' ); ?>" class="button button-secondary" />
-		</div><!-- .theme-switcher-wrap -->
-		<div class="standards-wrap">
-			<h2><?php esc_html_e( 'Select Standard', 'ns-theme-check' ); ?></h2>
-			<?php foreach ( $standards as $key => $standard ) : ?>
-				<label for="<?php echo esc_attr( $key ); ?>" title="<?php echo esc_attr( $standard['description'] ); ?>">
-					<input type="checkbox" name="<?php echo esc_attr( $key ); ?>" id="<?php echo esc_attr( $key ); ?>" value="1" <?php checked( $standard_status[ $key ], 1 ); ?> />
-					<?php echo '<strong>' . esc_html( $standard['label'] ) . '</strong>: ' . esc_html( $standard['description'] ); ?>
-				</label><br>
-			<?php endforeach; ?>
-		</div><!-- .standards-wrap -->
-		<div class="options-wrap">
-			<h2><?php esc_html_e( 'Options', 'ns-theme-check' ); ?></h2>
-			<label for="hide_warning"><input type="checkbox" name="hide_warning" id="hide_warning" value="1" <?php checked( $hide_warning, 1 ); ?> /><?php esc_html_e( 'Hide Warning', 'ns-theme-check' ); ?></label>
-			&nbsp;<label for="raw_output"><input type="checkbox" name="raw_output" id="raw_output" value="1" <?php checked( $raw_output, 1 ); ?> /><?php esc_html_e( 'Raw Output', 'ns-theme-check' ); ?></label>&nbsp;
-			<?php $php_versions = ns_theme_check_get_php_versions(); ?>
-			<label for="minimum_php_version">
-				<select name="minimum_php_version">
-				<?php foreach ( $php_versions as $version ) : ?>
-					<option value="<?php echo esc_attr( $version ); ?>" <?php selected( $minimum_php_version, $version ); ?>><?php echo esc_html( $version ); ?></option>
-				<?php endforeach; ?>
-				</select>
-				<?php esc_html_e( 'Minimum PHP Version', 'ns-theme-check' ); ?>
-			</label>
-		</div><!-- .options-wrap -->
-	</form>
-	<?php
-}
-
-/**
- * Render sniff results.
- *
- * @since 0.1.0
- */
-function ns_theme_check_render_output() {
-
-	$standards = ns_theme_check_get_standards();
-
-	// Bail if empty.
-	if ( empty( $_POST['themename'] ) ) {
-		return;
-	}
-
-	// Verify nonce.
-	if ( ! isset( $_POST['ns_theme_check_nonce'] ) || ! wp_verify_nonce( $_POST['ns_theme_check_nonce'], 'ns_theme_check_run' ) ) {
-		esc_html_e( 'Error', 'ns-theme-check' );
-		return;
-	}
-
-	$args = array(
-		'show_warnings' => 1,
-		'raw_output'    => 0,
-	);
-
-	if ( isset( $_POST['hide_warning'] ) && 1 === absint( $_POST['hide_warning'] ) ) {
-		$args['show_warnings'] = 0;
-	}
-
-	if ( isset( $_POST['raw_output'] ) && 1 === absint( $_POST['raw_output'] ) ) {
-		$args['raw_output'] = 1;
-	}
-
-	if ( isset( $_POST['minimum_php_version'] ) && ! empty( $_POST['minimum_php_version'] ) ) {
-		$args['minimum_php_version'] = esc_html( $_POST['minimum_php_version'] );
-	}
-
-	$args['standard'] = array();
-	foreach ( $standards as $key => $standard ) {
-		if ( isset( $_POST[ $key ] ) && 1 === absint( $_POST[ $key ] ) ) {
-			$args['standard'][] = $standard['label'];
-		}
-	}
-
-	ns_theme_check_do_sniff( esc_html( $_POST['themename'] ), $args );
-
-}
-
-/**
- * Perform sniff check.
- *
- * @since 0.1.0
- *
- * @param string $theme_slug Theme slug.
- * @param array  $args Arguments.
- *
- * @return bool
- */
-function ns_theme_check_do_sniff( $theme_slug, $args = array() ) {
-
-	if ( ! file_exists( NS_THEME_CHECK_DIR . '/vendor/autoload.php' ) ) {
-		printf( esc_html__( 'It seems you are using GitHub provided zip for the plugin. Visit %1$sInstalling%2$s to find the correct bundled plugin zip.', 'ns-theme-check' ), '<a href="https://github.com/ernilambar/ns-theme-check#installing" target="_blank">', '</a>' );
-		return;
-	}
-
-	require_once NS_THEME_CHECK_DIR . '/vendor/autoload.php';
-
-	// Path to WordPress Theme coding standard.
-	PHP_CodeSniffer::setConfigData( 'installed_paths', NS_THEME_CHECK_DIR . '/vendor/wp-coding-standards/wpcs/', true );
-	PHP_CodeSniffer::setConfigData( 'csslint_path', NS_THEME_CHECK_DIR . '/node_modules/csslint/dist/cli.js --errors=errors', true );
-
-	// Set default standard.
-	PHP_CodeSniffer::setConfigData( 'default_standard', 'WordPress-Theme', true );
-
-	// Ignoring warnings when generating the exit code.
-	PHP_CodeSniffer::setConfigData( 'ignore_warnings_on_exit', true, true );
-
-	// Set text domains.
-	$theme = wp_get_theme( $theme_slug );
-	$text_domains = array(
-		// Current theme text domain.
-		$theme_slug,
-		// Frameworks.
-		'hybrid-core',
-	);
-	PHP_CodeSniffer::setConfigData( 'text_domain', implode( ',', $text_domains ), true );
-
-	if ( isset( $args['show_warnings'] ) ) {
-		PHP_CodeSniffer::setConfigData( 'show_warnings', absint( $args['show_warnings'] ), true );
-	}
-
-	$minimum_php = '5.2';
-	if ( isset( $args['minimum_php_version'] ) && ! empty( $args['minimum_php_version'] ) ) {
-		$minimum_php = $args['minimum_php_version'];
-	}
-
-	PHP_CodeSniffer::setConfigData( 'testVersion', $minimum_php . '-7.0', true );
-
-	// Initialise CodeSniffer.
-	$phpcs_cli = new PHP_CodeSniffer_CLI();
-	$phpcs_cli->checkRequirements();
-
-	// Set CLI arguments.
-	$values['files']       = get_theme_root() . '/' . $theme_slug;
-	$values['reportWidth'] = '110';
-
-	if ( isset( $args['raw_output'] ) && 0 === absint( $args['raw_output'] ) ) {
-		$values['reports']['json'] = null;
-	}
-
-	if ( isset( $args['standard'] ) && ! empty( $args['standard'] ) ) {
-		$values['standard'] = $args['standard'];
-	}
-
-	$values['standard'][] = NS_THEME_CHECK_DIR . '/bin/phpcs.xml';
-
-	// Ignore unrelated files from the check.
-	$values['ignored'] = array(
-		'.*/node_modules/.*',
-	);
-
-	ob_start();
-	$num_errors = $phpcs_cli->process( $values );
-	$raw_output = ob_get_clean();
-
-	// Sniff theme files.
-	if ( isset( $args['raw_output'] ) && 1 === absint( $args['raw_output'] ) ) {
-		echo '<div class="theme-check-report theme-check-report-raw">';
-		ns_theme_check_show_repot_info();
-		echo '<pre>';
-		echo esc_html( $raw_output );
-		echo '</pre></div>';
-	} else {
-		$output = json_decode( $raw_output );
-		if ( ! empty( $output ) ) {
-			ns_theme_check_render_json_report( $output );
-		}
-	}
-
-	// Has the theme passed?
-	if ( 0 === $num_errors ) {
-		return true;
-	} else {
-		return false;
-	}
-
-}
-
-/**
  * Render JSON data in cleaner format.
  *
  * @since 0.1.0
@@ -257,97 +13,37 @@ function ns_theme_check_do_sniff( $theme_slug, $args = array() ) {
  * @param string $json JSON data.
  */
 function ns_theme_check_render_json_report( $json ) {
+
+	if ( ! isset( $json->files ) ) {
+		return;
+	}
 	?>
-	<div class="theme-check-report theme-check-report-json">
-		<?php ns_theme_check_show_repot_info(); ?>
-
-		<table class="report-summary">
-			<tr class="heading">
-				<th colspan="2"><?php esc_html_e( 'Summary', 'ns-theme-check' ); ?></th>
-			</tr>
-			<tr class="field">
-				<td><?php esc_html_e( 'Errors', 'ns-theme-check' ); ?></td>
-				<td><?php esc_html_e( 'Warnings', 'ns-theme-check' ); ?></td>
-			</tr>
-			<tr>
-				<td><?php echo absint( $json->totals->errors ); ?></td>
-				<td><?php echo absint( $json->totals->warnings ); ?></td>
-			</tr>
-		</table><!-- .summary -->
-
+	<?php foreach ( $json->files as $file_key => $file ) : ?>
 		<?php
-		$files = '';
-		if ( isset( $json->files ) ) {
-			$files = $json->files;
+		if ( 0 === absint( $file->errors ) && 0 === absint( $file->warnings ) ) {
+			continue;
 		}
+		$exploded_file_path = explode( '/themes/', $file_key );
+		$file_name = array_pop( $exploded_file_path );
 		?>
-
-		<?php if ( $files ) : ?>
-
-			<div class="report-files">
-
-				<?php foreach ( $files as $file_key => $file ) : ?>
-
-					<?php
-					$errors   = $file->errors;
-					$warnings = $file->warnings;
-
-					if ( 0 === absint( $errors ) && 0 === absint( $warnings ) ) {
-						continue;
-					}
-					?>
-
-					<div class="report-file-item">
-						<div class="report-file-heading">
-							<span class="heading-field"><?php esc_html_e( 'File:', 'ns-theme-check' ); ?></span>
-							<?php
-								$exploded_file_path = explode( '/themes/', $file_key );
-								$file_name = array_pop( $exploded_file_path );
-							?>
-							<?php echo esc_html( $file_name ); ?>
-
-						</div><!-- .report-file-heading -->
-						<div class="report-file-heading-meta">
-							<?php echo sprintf( esc_html__( '%1$d errors and %2$d warnings' ), absint( $errors ), absint( $warnings ) ); ?>
-						</div><!-- .report-file-heading-meta -->
-
-						<?php if ( ! empty( $file->messages ) && is_array( $file->messages ) ) : ?>
-
-							<table class="report-table">
-								<?php $cnt = 1; ?>
-								<?php foreach ( $file->messages as $item ) : ?>
-									<?php $row_class = ( 'error' === strtolower( $item->type ) ) ? 'item-type-error' : 'item-type-warning'; ?>
-									<tr class="<?php echo esc_attr( $row_class ); ?>">
-										<td class="td-line"><?php printf( esc_html__( 'Line: %d', 'ns-theme-check' ), absint( $item->line ) ); ?></td>
-										<td class="td-type"><?php echo esc_html( $item->type ); ?></td>
-										<td class="td-message"><?php echo esc_html( $item->message ); ?></td>
-									</tr>
-									<?php $cnt++; ?>
-								<?php endforeach; ?>
-							</table>
-
-						<?php endif; ?>
-
-					</div><!-- .report-file-item -->
-
-				<?php endforeach; ?>
-			</div><!-- .report-files -->
-
-		<?php endif; ?>
-
-	</div><!-- .theme-check-report .theme-check-report-json -->
-	<?php
-}
-
-/**
- * Show info about report.
- *
- * @since 0.1.2
- */
-function ns_theme_check_show_repot_info() {
-	?>
-	<p><strong><?php esc_html_e( 'Note: Errors need to be fixed and Warnings are things that need to be checked manually.', 'ns-theme-check' ); ?></strong></p>
-	<hr />
+		<div class="report-file-item">
+			<div class="report-file-heading">
+				<span class="report-file-heading-field"><?php printf( esc_html__( 'File: %s','ns-theme-check' ), esc_html( $file_name ) ); ?></span><!-- .report-file-heading-field -->
+			</div><!-- .report-file-heading -->
+			<?php if ( ! empty( $file->messages ) && is_array( $file->messages ) ) : ?>
+				<table class="report-table">
+					<?php foreach ( $file->messages as $item ) : ?>
+						<?php $row_class = ( 'error' === strtolower( $item->type ) ) ?'item-type-error' : 'item-type-warning'; ?>
+						<tr class="<?php echo esc_attr( $row_class ); ?>">
+							<td class="td-line"><?php printf( esc_html__( 'Line: %d','ns-theme-check' ), absint( $item->line ) ); ?></td>
+							<td class="td-type"><?php echo esc_html( $item->type ); ?></td>
+							<td class="td-message"><?php echo esc_html( $item->message ); ?></td>
+						</tr>
+					<?php endforeach; ?>
+				</table>
+			<?php endif; ?>
+		</div><!-- .report-file-item -->
+	<?php endforeach; ?>
 	<?php
 }
 
@@ -391,6 +87,7 @@ function ns_theme_check_get_standards() {
 	return $output;
 
 }
+
 /**
  * Returns PHP versions.
  *
@@ -411,4 +108,58 @@ function ns_theme_check_get_php_versions() {
 
 	return $output;
 
+}
+
+/**
+ * Returns theme tags.
+ *
+ * @since 0.1.3
+ *
+ * @return array PHP versions.
+ */
+function ns_theme_check_get_theme_tags() {
+
+	$tags['allowed_tags'] = array(
+		'grid-layout'           => 'grid-layout',
+		'one-column'            => 'one-column',
+		'two-columns'           => 'two-columns',
+		'three-columns'         => 'three-columns',
+		'four-columns'          => 'four-columns',
+		'left-sidebar'          => 'left-sidebar',
+		'right-sidebar'         => 'right-sidebar',
+		'flexible-header'       => 'flexible-header',
+		'footer-widgets'        => 'footer-widgets',
+		'accessibility-ready'   => 'accessibility-ready',
+		'buddypress'            => 'buddypress',
+		'custom-background'     => 'custom-background',
+		'custom-colors'         => 'custom-colors',
+		'custom-header'         => 'custom-header',
+		'custom-menu'           => 'custom-menu',
+		'custom-logo'           => 'custom-logo',
+		'editor-style'          => 'editor-style',
+		'featured-image-header' => 'featured-image-header',
+		'featured-images'       => 'featured-images',
+		'front-page-post-form'  => 'front-page-post-form',
+		'full-width-template'   => 'full-width-template',
+		'microformats'          => 'microformats',
+		'post-formats'          => 'post-formats',
+		'rtl-language-support'  => 'rtl-language-support',
+		'sticky-post'           => 'sticky-post',
+		'theme-options'         => 'theme-options',
+		'threaded-comments'     => 'threaded-comments',
+		'translation-ready'     => 'translation-ready',
+	);
+	$tags['subject_tags'] = array(
+		'blog'           => 'blog',
+		'e-commerce'     => 'e-commerce',
+		'education'      => 'education',
+		'entertainment'  => 'entertainment',
+		'food-and-drink' => 'food-and-drink',
+		'holiday'        => 'holiday',
+		'news'           => 'news',
+		'photography'    => 'photography',
+		'portfolio'      => 'portfolio',
+	);
+
+	return $tags;
 }
