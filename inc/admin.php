4687120d273205e2e932e6260ab965836c8f50de
dingo-d <denis.zoljom@gmail.com>
2017-10-08 11:29:34 +0200

    Remove usage of admin-ajax and use REST API instead

--- inc/admin.php
+++ inc/admin.php
@@ -64,11 +64,12 @@ function theme_sniffer_admin_scripts( $hook ) {
 	}
 	wp_enqueue_style( 'theme-sniffer-admin', THEME_SNIFFER_URL . '/css/admin.css', array(), '0.1.3c' );
 	wp_enqueue_script( 'theme-sniffer-admin', THEME_SNIFFER_URL . '/js/admin.js', array( 'jquery', 'underscore' ), '0.1.4' );
-	wp_localize_script( 'theme-sniffer-admin', 'localization_object', array(
-		'sniff_error' => __( 'The check has failed. This could happen due to running out of memory. Either reduce the file length or increase PHP memory.', 'theme-sniffer' ),
+	wp_localize_script( 'theme-sniffer-admin', 'localizationObject', array(
+		'sniff_error'      => __( 'The check has failed. This could happen due to running out of memory. Either reduce the file length or increase PHP memory.', 'theme-sniffer' ),
 		'percent_complete' => __( 'Percent completed: ', 'theme-sniffer' ),
-		'check_starting' => __( 'Check starting...', 'theme-sniffer' ),
-		'check_failed' => __( 'Check has failed :(', 'theme-sniffer' ),
+		'check_starting'   => __( 'Check starting...', 'theme-sniffer' ),
+		'check_failed'     => __( 'Check has failed :(', 'theme-sniffer' ),
+		'check_done'     	 => __( 'All done!', 'theme-sniffer' ),
 	));
 }
 add_action( 'admin_enqueue_scripts', 'theme_sniffer_admin_scripts' );
@@ -128,7 +129,7 @@ function theme_sniffer_render_form() {
 	}
 	?>
 	<form action="<?php echo esc_url( admin_url( 'themes.php?page=theme-sniffer' ) ); ?>" method="post" class="frm-theme-sniffer">
-		<?php wp_nonce_field( 'theme_sniffer_run', 'theme_sniffer_nonce' ); ?>
+		<?php wp_nonce_field( 'wp_rest', 'theme_sniffer_nonce' ); ?>
 		<div class="theme-switcher-wrap">
 			<h2><?php esc_html_e( 'Select Theme', 'theme-sniffer' ); ?></h2>
 			<label for="themename">
@@ -166,92 +167,4 @@ function theme_sniffer_render_form() {
 	</form>
 	<div class="theme-sniffer-report"></div><!-- .theme-sniffer-report -->
 	<?php
-}
-
-add_action( 'wp_ajax_theme_sniffer_run', 'theme_sniffer_initialize_sniff' );
-/**
- * Start sniffing
- *
- * @since 0.1.0
- */
-function theme_sniffer_initialize_sniff() {
-	// Bail if empty.
-	if ( empty( $_POST['themename'] ) ) {
-		return;
-	}
-
-	if ( ! file_exists( THEME_SNIFFER_DIR . '/vendor/autoload.php' ) ) {
-		$message = sprintf( esc_html__( 'It seems you are using GitHub provided zip for the plugin. Visit %1$sInstalling%2$s to find the correct bundled plugin zip.', 'theme-sniffer' ), '<a href="https://github.com/ernilambar/theme-sniffer#installing" target="_blank">', '</a>' );
-		$error = new WP_Error( '-1', $message );
-		wp_send_json_error( $error );
-	}
-
-	$theme_slug = esc_html( $_POST['themename'] );
-
-	// Verify nonce.
-	if ( ! isset( $_POST['theme_sniffer_nonce'] ) || ! wp_verify_nonce( $_POST['theme_sniffer_nonce'], 'theme_sniffer_run' ) ) {
-		esc_html_e( 'Error', 'theme-sniffer' );
-		return;
-	}
-
-	if ( isset( $_POST['hide_warning'] ) && 'true' === $_POST['hide_warning'] ) {
-		$args['show_warnings'] = true;
-	}
-
-	if ( isset( $_POST['raw_output'] ) && 'true' === $_POST['raw_output'] ) {
-		$args['raw_output'] = 1;
-	}
-
-	if ( isset( $_POST['minimum_php_version'] ) && ! empty( $_POST['minimum_php_version'] ) ) {
-		$args['minimum_php_version'] = esc_html( $_POST['minimum_php_version'] );
-	}
-
-	$standards = theme_sniffer_get_standards();
-	foreach ( $standards as $key => $standard ) {
-		if ( isset( $_POST[ $key ] ) && 'true' === $_POST[ $key ] ) {
-			$args['standard'][] = $standard['label'];
-		}
-	}
-
-	$theme = wp_get_theme( $theme_slug );
-	$php_files = $theme->get_files( 'php', 4, false );
-	// Current theme text domain.
-	$args['text_domains'][] = $theme_slug;
-	// Frameworks.
-	foreach ( $php_files as $key => $file ) {
-		if ( strrpos( $key, 'hybrid.php' ) ) {
-			$args['text_domains'][] = 'hybrid-core';
-		}
-		if ( strrpos( $key, 'kirki.php' ) ) {
-			$args['text_domains'][] = 'kirki';
-		}
-	}
-
-	$all_files = $theme->get_files( array( 'php', 'css,', 'js' ), -1, false );
-
-	wp_send_json_success( array( $theme_slug, $args, $all_files ) );
-
-}
-
-add_action( 'wp_ajax_theme_sniffer_sniff', 'theme_sniffer_individual_files' );
-/**
- * Render sniff results.
- *
- * @since 0.1.0
- */
-function theme_sniffer_individual_files() {
-	// Bail if empty.
-	if ( empty( $_POST['theme_name'] ) || empty( $_POST['theme_args'] ) || empty( $_POST['file'] ) ) {
-		return;
-	}
-
-	// Verify nonce.
-	if ( ! isset( $_POST['theme_sniffer_nonce'] ) || ! wp_verify_nonce( $_POST['theme_sniffer_nonce'], 'theme_sniffer_run' ) ) {
-		return;
-	}
-
-	$sniff = theme_sniffer_do_sniff( $_POST['theme_name'], $_POST['theme_args'], $_POST['file'] );
-
-	wp_send_json_success( $sniff );
-
 }
