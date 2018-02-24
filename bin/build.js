07432f00d3116b05f20bca9f521ae3161c80081f
dingo-d <denis.zoljom@gmail.com>
2018-02-24 10:23:14 +0100

    Updated configs and packages

--- bin/build.js
+++ bin/build.js
@@ -1,21 +1,44 @@
 #!/usr/bin/env node
 
-const fse = require( 'fs-extra' );
+const fs = require( 'fs' );
 const path = require( 'path' );
 
-const dir = '/build';
+// Helper methods
+const mkdir = (dir) => {
+	// making directory without exception if exists
+	try {
+		// fs.mkdirSync(dir, 0755);
+	} catch(e) {
+		if(e.code != "EEXIST") {
+			throw e;
+		}
+	}
+};
+
+
+const copyDir = (src, dest) => {
+	mkdir(dest);
+	const files = fs.readdirSync(src);
+	for(let i = 0; i < files.length; i++) {
+		const current = fs.lstatSync(path.join(src, files[i]));
+		if(current.isDirectory()) {
+			copyDir(path.join(src, files[i]), path.join(dest, files[i]));
+		} else if(current.isSymbolicLink()) {
+			const symlink = fs.readlinkSync(path.join(src, files[i]));
+			fs.symlinkSync(symlink, path.join(dest, files[i]));
+		} else {
+			copy(path.join(src, files[i]), path.join(dest, files[i]));
+		}
+	}
+};
+
+const copy = (src, dest) => {
+	const oldFile = fs.createReadStream(src);
+	const newFile = fs.createWriteStream(dest);
+	util.pump(oldFile, newFile);
+};
 
 
 // Copy the plugin to build folder in a new folder, then delete stuff that is not needed in production
 
-// With async/await:
-async function example( directory ) {
-	try {
-		await fse.ensureDir( directory );
-		console.log( 'build/ directory created!' );
-	} catch ( err ) {
-		console.error( err );
-	}
-}
-
-example( dir );
+mkdir('./build');
