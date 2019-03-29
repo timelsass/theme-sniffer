c3fc0fa95c87b860384b13443ad442e8c59f0633
Tim <dev@tim.ph>
2019-03-29 16:57:31 -0400

    pull in license data dependency

--- bin/build.js
+++ bin/build.js
@@ -1,21 +1,5 @@
 const fs = require( 'fs' );
 const path = require( 'path' );
+const licenses = require( 'wp-license-compatibility' );
 
-( async () => {
-	const read = fs.createReadStream( path.resolve( path.join( __dirname, '..', 'assets', 'dev', 'licenses.json' ) ) );
-	const write = fs.createWriteStream( path.resolve( path.join( __dirname, '..', 'assets', 'build', 'licenses.json' ) ) );
-
-	try {
-		return await new Promise( ( resolve, reject ) => {
-			read.on( 'error', reject );
-			write.on( 'error', reject );
-			write.on( 'finish', resolve );
-			read.pipe( write );
-		} );
-	} catch ( error ) {
-		read.destroy();
-		write.end();
-		throw error;
-	}
-
-} )().catch( console.error );
+fs.writeFile( path.resolve( path.join( __dirname, '..', 'assets', 'build', 'licenses.json' ) ), JSON.stringify( licenses, null, 2 ), err => console.error );
